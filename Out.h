d3fe90f36bf3050668b454004c4b1d3e1111e9c4
Vladislav <63453224+VLARINT3G@users.noreply.github.com>
2025-02-27 23:21:16 +0300

    Update Out.h

--- Out.h
+++ Out.h
@@ -1,11 +1,13 @@
+#pragma once
+#include <Input.h>
+#include <string>
+
 /**
  * @file Out.h
  * @brief Класс для вывода текста на экран и сохранения его в файл.
  */
 
-#pragma once
-#include <Input.h>
-#include <string>
+
 
 class Out {
 public:
