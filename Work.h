add39dcf375b9055607d0d8dd8a96df2052ee568
Vladislav <63453224+VLARINT3G@users.noreply.github.com>
2025-02-27 23:22:06 +0300

    Update Work.h

--- Work.h
+++ Work.h
@@ -1,10 +1,10 @@
+#pragma once
+#include <Input.h>
+
 /**
  * @file Work.h
  * @brief Класс для обработки текста на основе параметров ввода.
  */
-
-#pragma once
-#include <Input.h>
 
 class Work {
 public:
