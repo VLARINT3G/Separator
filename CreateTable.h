618bd17c6e1d720a42c2e46f6bfd8de77311c125
Vladislav <63453224+VLARINT3G@users.noreply.github.com>
2025-02-27 22:35:09 +0300

    Update CreateTable.h

--- CreateTable.h
+++ CreateTable.h
@@ -1,24 +1,46 @@
-/**
- * @file CreateTable.h
- * @brief Заголовочный файл для создания таблиц в базе данных.
- */
-
 #pragma once
 
 #include <string>
 #include <vector>
+#include <Separator.h>
 
 /**
- * @brief Класс для создания таблиц в базе данных.
+ * @brief Определение типа для названия таблицы.
+ */
+using TableName = std::string;
+
+/**
+ * @brief Определение типа для списка столбцов таблицы.
+ */
+using ColumnList = std::vector<std::string>;
+
+/**
+ * @class CreateTable
+ * @brief Класс для создания SQL-запроса создания таблицы.
  */
 class CreateTable {
-public:
-    explicit CreateTable(const std::string& tableName);
+ public:
+  /**
+   * @brief Конструктор класса CreateTable.
+   * @param tableName Название таблицы.
+   */
+  explicit CreateTable(const TableName &tableName);
 
-    void AddTextColumn(const std::string& columnName);
-    std::string GetCreateQuery() const;
+  /**
+   * @brief Добавляет в структуру таблицы новую колонку типа TEXT.
+   * @details Метод не выполняет SQL-запрос, а лишь сохраняет имя колонки,
+   * чтобы затем использовать его в SQL-операции CREATE TABLE.
+   * @param columnName Имя добавляемой колонки.
+   */
+  void AddStringColumn(const std::string &columnName);
 
-private:
-    std::string tableName;
-    std::vector<std::string> columns;
+  /**
+   * @brief Генерирует SQL-запрос для создания таблицы.
+   * @return Строка с SQL-запросом.
+   */
+  std::string GetCreateQuery() const;
+
+ private:
+  TableName tableName_;  ///< Название таблицы.
+  ColumnList columns_;  ///< Список столбцов таблицы.
 };
